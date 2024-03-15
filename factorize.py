f5b5ee69f10b83858b021e61ce422468a15925cd
eternityspace <rodon@protonmail.ch>
2024-03-15 22:04:50 +0200

    fixed buggs

--- factorize.py
+++ factorize.py
@@ -1,4 +1,5 @@
 ﻿from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import cpu_count
 from time import time
 
 result_factorize = []
@@ -12,17 +13,20 @@ def factorize(number: int) -> list:
     return factors
 
 
-def process_factorize(numbers: list) -> list:
+def process_factorize(*numbers: list) -> list:
     result = []
-    with ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(max_workers=cpu_count()) as executor:
         factorize_result = list(executor.map(factorize, numbers))
-        for l in factorize_result:
-            result += l
-        return list(set(result))
+        # for l in factorize_result:
+        #     result += l
+        # return list(set(result))
+
+        return factorize_result
 
 
 if __name__ == '__main__':
     start_time = time()
-    print(process_factorize([1, 2, 4, 5, 8, 1000222]))
+    # print(process_factorize([1, 2, 4, 5, 8, 1000222]))
+    print(process_factorize(128, 256, 10651060))
     end_time = time()
     print(end_time - start_time)
